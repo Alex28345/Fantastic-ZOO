684e2f7f2c1b5a5e093f4dfd827b26d2de790618
Alexandre possenti <alex.possenti0@gmail.com>
2023-11-13 22:08:32 +0100

    test et modif

--- src/main/java/fr/fantasticzoo/creatures/Creature.java
+++ src/main/java/fr/fantasticzoo/creatures/Creature.java
@@ -10,10 +10,22 @@ public abstract class Creature {
     protected boolean isSleeping;
     protected int health;
 
-    public void feed(){}
-    public void makeSound(){}
-    public void heal(){}
-    public void sleep(){}
-    public void wakeUp(){}
-    public void age(){}
+    public void feed(){
+        System.out.println("I'm eating");
+    }
+    public void makeSound(){
+        System.out.println("I'm making sound");
+    }
+    public void heal(){
+        System.out.println("I'm healing");
+    }
+    public void sleep(){
+        System.out.println("I'm sleeping");
+    }
+    public void wakeUp(){
+        System.out.println("I'm waking up");
+    }
+    public void age(){
+        System.out.println("I'm aging");
+    }
 }
---
+++ src/test/java/fr/fantasticzoo/creatures/CreatureTest.java
@@ -0,0 +1,32 @@
+package fr.fantasticzoo.creatures;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreatureTest {
+
+    @Test
+    void feed() {
+    }
+
+    @Test
+    void makeSound() {
+    }
+
+    @Test
+    void heal() {
+    }
+
+    @Test
+    void sleep() {
+    }
+
+    @Test
+    void wakeUp() {
+    }
+
+    @Test
+    void age() {
+    }
+}
