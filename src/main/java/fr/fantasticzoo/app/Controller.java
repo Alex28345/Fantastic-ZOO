d9865b7b333d69903215c6b3595913ffe8f83884
sookdp <sokhna.diop.1@etu.univ-amu.fr>
2023-11-23 11:10:57 +0100

    Création d'un nouveau controller et d'une page fxml et donc lorsque l'on sélectionne un enclos, le nouveau controller est appelé et on a une vue d'ensemble de l'enclos sélectionné

--- src/main/java/fr/fantasticzoo/app/Controller.java
+++ src/main/java/fr/fantasticzoo/app/Controller.java
@@ -4,10 +4,17 @@ import fr.fantasticzoo.Zoo;
 import fr.fantasticzoo.enclosures.Enclosure;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Controller {
     private Zoo zoo;
@@ -23,9 +30,7 @@ public class Controller {
     private ComboBox selectedCreature;
     @FXML
     private FlowPane enclosures;
-
-    @FXML Button enclosure;
-
+    Button newButton = new Button();
     @FXML
     public void createEnclosure() {
         Enclosure enclosure = new Enclosure("enclos "+i, 100, 100);
@@ -33,9 +38,7 @@ public class Controller {
         Button newButton = new Button(enclosure.getName());
         newButton.setText(enclosure.getName());
         enclosures.getChildren().add(newButton);
-        newButton.setOnAction(e -> {
-            System.out.println("Bouton cliqué");
-        });
+        newButton.setOnAction(this::handleButtonAction);
         i++;
         for(Enclosure enclosure1 : zoo.getEnclosures()){
             if (enclosure1 != null){
@@ -43,6 +46,20 @@ public class Controller {
             }
         }
         System.out.println(' ');
+    }
+
+    private void handleButtonAction(ActionEvent actionEvent) {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("enclosureView.fxml"));
+        try {
+            Parent root = loader.load();
+            Scene currentScene = ((Node) actionEvent.getSource()).getScene();
+            Scene newScene = new Scene(root);
+            Stage stage = (Stage) currentScene.getWindow();
+            stage.setScene(newScene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void inspectEnclosure(ActionEvent actionEvent) {
