79c4bf1453db42b47545774a963e6f493f07f14d
Myke Lieto <myke.lieto@etu.univ-amu.fr>
2023-12-03 17:14:13 +0100

    Lycanthrope : Domination

--- src/main/java/fr/fantasticzoo/creatures/Lycanthropes/Lycanthrope.java
+++ src/main/java/fr/fantasticzoo/creatures/Lycanthropes/Lycanthrope.java
@@ -3,11 +3,14 @@ package fr.fantasticzoo.creatures.Lycanthropes;
 import fr.fantasticzoo.creatures.abstractClasses.Viviparous;
 import fr.fantasticzoo.creatures.propertiesInterfaces.Flyer;
 import fr.fantasticzoo.creatures.propertiesInterfaces.Runner;
+import fr.fantasticzoo.enums.DominationRank;
 
 public class Lycanthrope extends Viviparous implements Runner {
+    //Lorsque le facteur de domination est en dessous d’un certain seuil, le lycanthrope perd naturellement un rang de domination s’il n’est pas le dernier de son sexe dans la meute à l’avoir (par exemple : le dernier mâle β d’une meute ne pourra pas devenir un mâle γ).
     private int strength;
+    //Les lycanthropes ω sont des lycanthropes adultes considérés comme ayant une force insuffisante par rapport à la moyenne du groupe.
     private int dominationFactor;
-    private int rankDomination;
+    private DominationRank rankDomination;
     private int level;
     private int impetuosityFactor;
     private Pack pack;
@@ -35,11 +38,53 @@ public class Lycanthrope extends Viviparous implements Runner {
     @Override
     public void run() {}
 
-    public void scream(){}
+    public void scream(){
+        System.out.println("Je cri");
+    }
 
-    public void hearScream(){}
+    public void hearScream(){
+        System.out.println("J'entend un cri");
+    }
 
-    public void separatePack(){}
+    public void separatePack(){
+        System.out.println("Je me sépare de ma meute");
+    }
 
-    public void humanTransformation(){}
+    public void humanTransformation(){
+        System.out.println("Je me transforme en humain");
+    }
+
+    public void domination(Lycanthrope victim){
+        if (victim.getStrength() <= this.getStrength() && victim.getRankDomination() != DominationRank.α){
+            if (this.getLevel() > victim.getLevel() || victim.getRankDomination() == DominationRank.ω){
+
+                this.setDominationFactor(this.getDominationFactor() +1);
+                victim.setDominationFactor(victim.getDominationFactor() -1);
+
+                DominationRank tmp = this.getRankDomination();
+                this.setRankDomination(victim.getRankDomination());
+                victim.setRankDomination(tmp);
+
+                System.out.println("J'ai dominé ma victime");
+            }
+            else {
+                this.setDominationFactor(this.getDominationFactor() -1);
+                System.out.println("Je n'ai pas réussi ma domination, ma victime m'a agressé");
+            }
+        }
+        else {
+            this.setDominationFactor(this.getDominationFactor() -1);
+            System.out.println("Je n'ai pas réussi ma domination, ma victime m'a agressé");
+        }
+    }
+
+    public DominationRank getRankDomination() { return rankDomination; }
+    public void setRankDomination(DominationRank rankDomination) { this.rankDomination = rankDomination; }
+
+    public int getStrength() { return strength; }
+
+    public int getLevel() { return level; }
+
+    public int getDominationFactor() { return dominationFactor; }
+    public void setDominationFactor(int dominationFactor) { this.dominationFactor = dominationFactor; }
 }
