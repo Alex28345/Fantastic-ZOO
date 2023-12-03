3d66237a758508ace20fccf48ea7721a1a78787f
Myke Lieto <myke.lieto@etu.univ-amu.fr>
2023-12-03 19:24:35 +0100

    Lycanthrope : Domination et omega

--- src/main/java/fr/fantasticzoo/creatures/Lycanthropes/Lycanthrope.java
+++ src/main/java/fr/fantasticzoo/creatures/Lycanthropes/Lycanthrope.java
@@ -3,12 +3,13 @@ package fr.fantasticzoo.creatures.Lycanthropes;
 import fr.fantasticzoo.creatures.abstractClasses.Viviparous;
 import fr.fantasticzoo.creatures.propertiesInterfaces.Flyer;
 import fr.fantasticzoo.creatures.propertiesInterfaces.Runner;
+import fr.fantasticzoo.enums.Age;
 import fr.fantasticzoo.enums.DominationRank;
+import fr.fantasticzoo.enums.Sex;
 
 public class Lycanthrope extends Viviparous implements Runner {
     //Lorsque le facteur de domination est en dessous d’un certain seuil, le lycanthrope perd naturellement un rang de domination s’il n’est pas le dernier de son sexe dans la meute à l’avoir (par exemple : le dernier mâle β d’une meute ne pourra pas devenir un mâle γ).
     private int strength;
-    //Les lycanthropes ω sont des lycanthropes adultes considérés comme ayant une force insuffisante par rapport à la moyenne du groupe.
     private int dominationFactor;
     private DominationRank rankDomination;
     private int level;
@@ -34,26 +35,23 @@ public class Lycanthrope extends Viviparous implements Runner {
                 ", health=" + health +
                 '}';
     }
-
     @Override
     public void run() {}
 
-    public void scream(){
-        System.out.println("Je cri");
-    }
 
-    public void hearScream(){
-        System.out.println("J'entend un cri");
-    }
+    public DominationRank getRankDomination() { return rankDomination; }
+    public void setRankDomination(DominationRank rankDomination) { this.rankDomination = rankDomination; }
 
-    public void separatePack(){
-        System.out.println("Je me sépare de ma meute");
-    }
+    public int getStrength() { return strength; }
 
-    public void humanTransformation(){
-        System.out.println("Je me transforme en humain");
-    }
+    public int getLevel() { return level; }
 
+    public int getDominationFactor() { return dominationFactor; }
+    public void setDominationFactor(int dominationFactor) { this.dominationFactor = dominationFactor; }
+
+    public void hearScream(){ System.out.println("J'entend un cri"); }
+    public void separatePack(){ System.out.println("Je me sépare de ma meute"); }
+    public void humanTransformation(){ System.out.println("Je me transforme en humain"); }
     public void domination(Lycanthrope victim){
         if (victim.getStrength() <= this.getStrength() && victim.getRankDomination() != DominationRank.α){
             if (this.getLevel() > victim.getLevel() || victim.getRankDomination() == DominationRank.ω){
@@ -78,13 +76,16 @@ public class Lycanthrope extends Viviparous implements Runner {
         }
     }
 
-    public DominationRank getRankDomination() { return rankDomination; }
-    public void setRankDomination(DominationRank rankDomination) { this.rankDomination = rankDomination; }
+    //Les lycanthropes ω sont des lycanthropes adultes considérés comme ayant une force insuffisante par rapport à la moyenne du groupe.
+    public boolean isOmega(){
+        if(this.getSex() == Sex.male && this.getAge() == Age.adult && this.getStrength() < pack.averageStrength){
+            this.setRankDomination(DominationRank.ω);
+            return true;
+        }
+        return false;
+    }
 
-    public int getStrength() { return strength; }
-
-    public int getLevel() { return level; }
-
-    public int getDominationFactor() { return dominationFactor; }
-    public void setDominationFactor(int dominationFactor) { this.dominationFactor = dominationFactor; }
+    public void scream(){
+        System.out.println("Je cri");
+    }
 }
